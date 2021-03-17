163ec502fd460f5b27e3032689abc69aa4900b7c
cameliadarie <75748894+cameliadarie@users.noreply.github.com>
2021-03-17 22:11:56 +0200

    Add files via upload

---
+++ Obligatoriu 4/School.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class School implements Comparable <School> {
+
+    String nume;
+    int capacity;
+
+    public School(String nume, int capacity) {
+        this.nume = nume;
+        this.capacity = capacity;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public int compareTo(School o) {
+        return this.nume.compareTo(o.nume);
+    }
+}
